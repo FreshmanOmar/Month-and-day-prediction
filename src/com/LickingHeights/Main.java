c2bb51a93f55297ecd40e4c9cdd84817d9e472ca
Omar <Omar.abdi.2022@student.lhschools.org>
2019-04-18 08:29:19 -0400

    Good Point
    I got to a point where everything works but i need to find out why i'm still not getting the right day

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,31 +6,98 @@ public class Main {
 
     public static void main(String[] args) {
         // declare
+        int h;//The Day of the week
+        int q;//The Day of the Month
+        int K;//The Year of the century
+        int J;//The Zero based century
+        int theMonth;//The Word Form of the Month
         Scanner input = new Scanner(System.in);
         //initializing
         System.out.println("give me Your exact birthday starting with the\n Day");
-        int h = input.nextInt();
-        int q;
-        do {
-            System.out.println("\n Month");
-            q = input.nextInt();
-        } while (q < 1 || q > 12);
+        q = input.nextInt();
+
+        do { System.out.println("\n Month");
+            theMonth = input.nextInt();
+        } while (theMonth < 1 || theMonth > 12);
+
         System.out.println("\n the year ");
-        int k = input.nextInt();
-        int j = k / 100;
-        System.out.println("Day:" + h + "Month:" + q + "Year:" + k + " ");
-        String WordMonth = Month(q);
-        System.out.println(WordMonth+" is the Month you were Born");
+        K = input.nextInt();
+
+        J = K/100;
+
+        System.out.println("Day:" + q + "Month:" + theMonth + "Year:" + K + " ");
+
+        String WordMonth = Month(theMonth);
+        System.out.println(WordMonth+" is the Month, you were Born in "+K+" ");
+
+       int m = NumMonth(theMonth);//the Number form of the month
+
+        K =K%100;
+       h = (q + (13*(m+1))/5 + K + K/4 + J/4 +5*J)%7;
+       System.out.println(h);
+       theday(h);
+      String Day = theday(h);
+              System.out.println("you were born a "+Day+" ");
     }
 
-    public static String Month(int q) {
-        switch (q) {
+    public static String theday(int h) {
+        switch(h){
+            case 0:
+               return "Saturday";
+            case 1:
+                return"Sunday";
+            case 2:
+                return"Monday";
+            case 3:
+                return"Tuesday";
+            case 4:
+                return"Wednesday";
+            case 5:
+                return"Thursday";
+            case 6:
+                return"Friday";
+                }
+        return null;
+    }
+    public static int NumMonth(int theMonth) {
+        switch (theMonth) {
+            case 1:
+                return 13;
+            case 2:
+               return 14;
+            case 3:
+               return 3;
+            case 4:
+                return 4;
+            case 5:
+                return 5;
+            case 6:
+                return 6;
+            case 7:
+                return 7;
+            case 8:
+                return 8;
+            case 9:
+                return 9;
+            case 10:
+                return 10;
+            case 11:
+                return 11;
+            case 12:
+                return 12;
+         default:
+                return 0; }
+
+    }
+
+    public static String Month(int theMonth) {
+        switch (theMonth) {
             case 1:
                 return "January";
             case 2:
-               return "February";
+                return "February";
             case 3:
-               return "March";
+                return "March";
             case 4:
                 return "April";
             case 5:
@@ -49,13 +116,84 @@ public class Main {
                 return "October";
             case 12:
                 return "December";
-         default:
-                return "unknown";
-
-        }
-
+            default:
+                return "unknown"; }
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
